c103b5f21eabbbf319079b89393dcdfa1cabb0d9
njanic <nemanja.janic@gmail.com>
2015-08-06 13:27:17 +0200

    Initial commit

---
+++ initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ruby212Shopifyapp::Application.config.secret_key_base = 'beb284d8eb6ad8240badedaa359ef44f6d832aba22bea620f8453a6207f773b9b691f67b7cb805450434758eec6519deddc5f2907f12bdb26e1cd3050e0a12c2'
---
+++ initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ruby212Shopifyapp::Application.config.session_store :cookie_store, key: '_ruby-2_1_2@shopifyapp_session'
